ae3bad583b2bba0e3589789fd27357c9d188391c
Marvin Erdmann <Marvin.Erdmann@bmw.de>
2025-08-21 17:54:33 +0200

    TspQuboMappingDnx inherits postprocess from TspQuboMapping. And SimulatedAnnealerDwave provides list instead of dict upstream.

--- src/quark_plugin_dwave/simulated_annealer_dwave.py
+++ src/quark_plugin_dwave/simulated_annealer_dwave.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 from typing import override
 
 import dwave.samplers
-import numpy as np
 
 from quark.core import Core, Data, Result
 from quark.interface_types import Other, Qubo
@@ -18,18 +17,8 @@ class SimulatedAnnealerDwave(Core):
 
     num_reads: int = 100
 
-    _first_run: bool = True
-
     @override
     def preprocess(self, data: Qubo) -> Result:
-        # if self._first_run:
-        #     self._first_run = False
-        #     return Sleep(data)
-        # else:
-        #     device = dwave.samplers.SimulatedAnnealingSampler()
-        #     self._result = device.sample_qubo(data.as_dict(), num_reads=self.num_reads)
-        #     return Data(None)
-
         device = dwave.samplers.SimulatedAnnealingSampler()
         q = data.as_dnx_qubo()
         self._result = device.sample_qubo(q, num_reads=self.num_reads)
@@ -37,4 +26,7 @@ class SimulatedAnnealerDwave(Core):
 
     @override
     def postprocess(self, data: Data) -> Result:
-        return Data(Other(self._result.lowest().first.sample))  # type: ignore
+        solution_list = []
+        for key in sorted(self._result.lowest().first.sample.keys()):
+            solution_list.append(self._result.lowest().first.sample[key])
+        return Data(Other(solution_list))  # type: ignore
